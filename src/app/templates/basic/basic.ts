60bad2d8edc26c5b6a916d9405892dde5b386126
Marycarmen <marycarmen.rivo@gmail.com>
2026-06-09 14:13:01 -0600

    Creación de componente Footer

--- src/app/templates/basic/basic.ts
+++ src/app/templates/basic/basic.ts
@@ -2,19 +2,19 @@ import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { generalService } from '../../shared/services/general';
-import { ButtonModule } from 'primeng/button'
-
+import { ButtonModule } from 'primeng/button';
+import { FooterComponent } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-basic',
-  imports: [RouterLink, RouterOutlet, ButtonModule],
+  imports: [RouterLink, RouterOutlet, ButtonModule, FooterComponent],
   templateUrl: './basic.html',
   styleUrl: './basic.scss',
 })
 export class Basic {
-  generalService: generalService = inject(generalService)
+  generalService: generalService = inject(generalService);
 
-  constructor(){
-    this.generalService.loadDataPortals()
+  constructor() {
+    this.generalService.loadDataPortals();
   }
 }
