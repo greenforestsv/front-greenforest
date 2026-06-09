12840fb17c205fb450a91f4a296747c991939b2b
Marycarmen <marycarmen.rivo@gmail.com>
2026-06-09 16:39:15 -0600

    Creación de página Not Found 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,10 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./templates/basic/basic').then((m) => m.Basic),
-    loadChildren: () => import('./templates/basic/basic.routes').then((m) => m.BasicRouter)
-  }
+    loadChildren: () => import('./templates/basic/basic.routes').then((m) => m.BasicRouter),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/not-found/not-found').then((m) => m.NotFoundComponent),
+  },
 ];
