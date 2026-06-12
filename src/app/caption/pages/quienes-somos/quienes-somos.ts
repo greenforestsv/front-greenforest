9de894d0044e1b0049153f4af925814cad37e23f
Marycarmen <marycarmen.rivo@gmail.com>
2026-06-12 16:42:56 -0600

    Mejora de estilo de página de quienes somos

--- src/app/caption/pages/quienes-somos/quienes-somos.ts
+++ src/app/caption/pages/quienes-somos/quienes-somos.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { PricingPlanes } from '../../../components/pricing-planes/pricing-planes';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-quienes-somos',
-  imports: [],
+  imports: [PricingPlanes, ButtonModule],
   templateUrl: './quienes-somos.html',
   styleUrl: './quienes-somos.css',
 })
