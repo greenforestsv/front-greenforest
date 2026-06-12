8238607621857aaa141c5dae88a00c6e37e5b51b
Marycarmen <marycarmen.rivo@gmail.com>
2026-06-12 16:38:10 -0600

    Mejora de estilo de card de plaza vacante

--- src/app/components/job-card/job-card.ts
+++ src/app/components/job-card/job-card.ts
@@ -2,14 +2,15 @@ import { Component, input } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { BadgeModule } from 'primeng/badge';
-import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'job-card',
   standalone: true,
-  imports: [ButtonModule, CardModule, BadgeModule, DecimalPipe],
+  imports: [ButtonModule, CardModule, BadgeModule],
   templateUrl: './job-card.html',
+  styleUrl: './job-card.css',
 })
 export class JobCardComponent {
   empleo = input.required<any>();
+  active = input(false);
 }
