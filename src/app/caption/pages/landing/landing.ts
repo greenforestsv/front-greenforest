2876d7c35c3f9b58e9bb8035a81a05252a26e68c
Marycarmen <marycarmen.rivo@gmail.com>
2026-06-16 01:32:48 -0600

    Creación de nuevo componente de empresas de landing page

--- src/app/caption/pages/landing/landing.ts
+++ src/app/caption/pages/landing/landing.ts
@@ -1,24 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { FlashData } from '../../components/flash-data/flash-data';
-import { CaptionService } from '../../services/caption-service';
-import { CarouselModule } from 'primeng/carousel';
 import { ButtonModule } from 'primeng/button';
-import { Analytics } from '../../interfaces/interface';
 import { TestimoniosComponent } from '../../../components/testimonios/testimonios';
 import { MisionVision } from '../../../components/mision-vision/mision-vision';
 import { PilaresFundamentales } from '../../../components/pilares-fundamentales/pilares-fundamentales';
 import { Hero } from '../../../components/hero/hero';
+import { Empresas } from '../../../components/empresas/empresas';
+import { Analytics } from '../../interfaces/interface';
+import { CaptionService } from '../../services/caption-service';
 
 @Component({
   selector: 'app-landing',
   imports: [
     FlashData,
-    CarouselModule,
     ButtonModule,
     Hero,
     TestimoniosComponent,
     MisionVision,
     PilaresFundamentales,
+    Empresas,
   ],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
@@ -26,28 +26,5 @@ import { Hero } from '../../../components/hero/hero';
 export class Landing {
   captionS: CaptionService = inject(CaptionService);
   analytics: Analytics = this.captionS.loadAnalytics();
-  responsiveOptions = [
-    {
-      breakpoint: '1400px',
-      numVisible: 2,
-      numScroll: 1,
-    },
-    {
-      breakpoint: '1199px',
-      numVisible: 3,
-      numScroll: 1,
-    },
-    {
-      breakpoint: '767px',
-      numVisible: 2,
-      numScroll: 1,
-    },
-    {
-      breakpoint: '575px',
-      numVisible: 1,
-      numScroll: 1,
-    },
-  ];
-
   constructor() {}
 }
