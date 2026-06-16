be93d5149200891755e3a03e058be42f1aab229f
Marycarmen <marycarmen.rivo@gmail.com>
2026-06-16 11:03:22 -0600

    Creación de componente de nueva sección de estadísticas de landing page

--- src/app/caption/pages/landing/landing.ts
+++ src/app/caption/pages/landing/landing.ts
@@ -1,30 +1,15 @@
-import { Component, inject } from '@angular/core';
-import { FlashData } from '../../components/flash-data/flash-data';
-import { ButtonModule } from 'primeng/button';
+import { Component } from '@angular/core';
 import { TestimoniosComponent } from '../../../components/testimonios/testimonios';
 import { MisionVision } from '../../../components/mision-vision/mision-vision';
 import { PilaresFundamentales } from '../../../components/pilares-fundamentales/pilares-fundamentales';
 import { Hero } from '../../../components/hero/hero';
 import { Empresas } from '../../../components/empresas/empresas';
-import { Analytics } from '../../interfaces/interface';
-import { CaptionService } from '../../services/caption-service';
+import { Estadisticas } from '../../../components/estadisticas/estadisticas';
 
 @Component({
   selector: 'app-landing',
-  imports: [
-    FlashData,
-    ButtonModule,
-    Hero,
-    TestimoniosComponent,
-    MisionVision,
-    PilaresFundamentales,
-    Empresas,
-  ],
+  imports: [Hero, TestimoniosComponent, MisionVision, PilaresFundamentales, Empresas, Estadisticas],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
-export class Landing {
-  captionS: CaptionService = inject(CaptionService);
-  analytics: Analytics = this.captionS.loadAnalytics();
-  constructor() {}
-}
+export class Landing {}
