ec8e12ece9aa9893e314f0d47f0e9b021f1bece0
Marycarmen <marycarmen.rivo@gmail.com>
2026-06-16 00:25:07 -0600

    Creación de eyebrow en hero de landing page

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [ButtonModule, NgOptimizedImage],
+  imports: [ButtonModule, RouterLink],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
