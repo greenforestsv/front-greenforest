59afa3c1ea51e4e04d1a2c007d9dee18750af04e
Marycarmen <marycarmen.rivo@gmail.com>
2026-06-16 18:53:58 -0600

    Estilos de botones e inputs aplicados desde archivo scss principal

--- src/app/components/estadisticas/estadisticas.ts
+++ src/app/components/estadisticas/estadisticas.ts
@@ -1,11 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { FlashData } from '../../caption/components/flash-data/flash-data';
 import { Analytics } from '../../caption/interfaces/interface';
 import { CaptionService } from '../../caption/services/caption-service';
 
 @Component({
   selector: 'app-estadisticas',
-  imports: [FlashData],
+  imports: [],
   templateUrl: './estadisticas.html',
   styleUrl: './estadisticas.scss',
 })
