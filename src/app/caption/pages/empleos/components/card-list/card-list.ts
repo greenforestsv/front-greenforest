f55638f98b2b97d434f36756149b8a564f2a21f0
Marycarmen <marycarmen.rivo@gmail.com>
2026-06-17 16:51:19 -0600

    Creación de componentes de card-list y card-detail para página de bolsa de empleos

---
+++ src/app/caption/pages/empleos/components/card-list/card-list.ts
@@ -0,0 +1,87 @@
+import { Component, signal } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-card-list',
+  imports: [AvatarModule, ButtonModule],
+  templateUrl: './card-list.html',
+  styleUrl: './card-list.scss',
+})
+export class CardList {
+  empleos = signal([
+    {
+      id: 1,
+      puesto: 'Desarrollador Senior Frontend (React/Angular)',
+      empresa: 'Tech Solutions Inc.',
+      ubicacion: 'Tegucigalpa',
+      modalidad: 'Tiempo Completo',
+      salario: 2000,
+      descripcion:
+        'Buscamos un desarrollador experto para liderar la migración de microfrontends usando arquitecturas reactivas modernas.',
+      tags: ['Angular 22', 'Tailwind', 'TypeScript'],
+      fecha: 'Hace 2 días',
+      active: true,
+    },
+    {
+      id: 2,
+      puesto: 'Diseñador UI/UX',
+      empresa: 'Creative Studio',
+      ubicacion: 'San Salvador',
+      modalidad: 'Híbrido',
+      salario: 1200,
+      descripcion:
+        'Únete a nuestro equipo creativo para diseñar soluciones complejas SaaS aplicadas a la industria Fintech.',
+      tags: ['Figma', 'SaaS', 'Design Systems'],
+      fecha: 'Hace 5 días',
+    },
+    {
+      id: 3,
+      puesto: 'Gerente de Marketing',
+      empresa: 'Saasify Global',
+      ubicacion: 'Remoto',
+      modalidad: 'Tiempo Completo',
+      salario: 1800,
+      descripcion:
+        'Encargado de planificar y optimizar estrategias de adquisición de clientes B2B mediante canales digitales.',
+      tags: ['SEO', 'Google Ads', 'Data Analytics'],
+      fecha: 'Ayer',
+    },
+    {
+      id: 3,
+      puesto: 'Gerente de Marketing',
+      empresa: 'Saasify Global',
+      ubicacion: 'Remoto',
+      modalidad: 'Tiempo Completo',
+      salario: 1800,
+      descripcion:
+        'Encargado de planificar y optimizar estrategias de adquisición de clientes B2B mediante canales digitales.',
+      tags: ['SEO', 'Google Ads', 'Data Analytics'],
+      fecha: 'Ayer',
+    },
+    {
+      id: 3,
+      puesto: 'Gerente de Marketing',
+      empresa: 'Saasify Global',
+      ubicacion: 'Remoto',
+      modalidad: 'Tiempo Completo',
+      salario: 1800,
+      descripcion:
+        'Encargado de planificar y optimizar estrategias de adquisición de clientes B2B mediante canales digitales.',
+      tags: ['SEO', 'Google Ads', 'Data Analytics'],
+      fecha: 'Ayer',
+    },
+    {
+      id: 3,
+      puesto: 'Gerente de Marketing',
+      empresa: 'Saasify Global',
+      ubicacion: 'Remoto',
+      modalidad: 'Tiempo Completo',
+      salario: 1800,
+      descripcion:
+        'Encargado de planificar y optimizar estrategias de adquisición de clientes B2B mediante canales digitales.',
+      tags: ['SEO', 'Google Ads', 'Data Analytics'],
+      fecha: 'Ayer',
+    },
+  ]);
+}
--- src/app/caption/pages/empleos/empleos.ts
+++ src/app/caption/pages/empleos/empleos.ts
@@ -1,90 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { AvatarModule } from 'primeng/avatar';
 import { Hero } from './components/hero/hero';
 import { Busqueda } from './components/busqueda/busqueda';
+import { CardList } from './components/card-list/card-list';
+import { CardDetail } from './components/card-detail/card-detail';
 
 @Component({
   selector: 'app-empleos',
   standalone: true,
-  imports: [ButtonModule, AvatarModule, Hero, Busqueda],
+  imports: [ButtonModule, AvatarModule, Hero, Busqueda, CardList, CardDetail],
   templateUrl: './empleos.html',
   styleUrl: './empleos.css',
 })
-export class Empleos {
-  empleos = signal([
-    {
-      id: 1,
-      puesto: 'Desarrollador Senior Frontend (React/Angular)',
-      empresa: 'Tech Solutions Inc.',
-      ubicacion: 'Tegucigalpa',
-      modalidad: 'Tiempo Completo',
-      salario: 2000,
-      descripcion:
-        'Buscamos un desarrollador experto para liderar la migración de microfrontends usando arquitecturas reactivas modernas.',
-      tags: ['Angular 22', 'Tailwind', 'TypeScript'],
-      fecha: 'Hace 2 días',
-      active: true,
-    },
-    {
-      id: 2,
-      puesto: 'Diseñador UI/UX',
-      empresa: 'Creative Studio',
-      ubicacion: 'San Salvador',
-      modalidad: 'Híbrido',
-      salario: 1200,
-      descripcion:
-        'Únete a nuestro equipo creativo para diseñar soluciones complejas SaaS aplicadas a la industria Fintech.',
-      tags: ['Figma', 'SaaS', 'Design Systems'],
-      fecha: 'Hace 5 días',
-    },
-    {
-      id: 3,
-      puesto: 'Gerente de Marketing',
-      empresa: 'Saasify Global',
-      ubicacion: 'Remoto',
-      modalidad: 'Tiempo Completo',
-      salario: 1800,
-      descripcion:
-        'Encargado de planificar y optimizar estrategias de adquisición de clientes B2B mediante canales digitales.',
-      tags: ['SEO', 'Google Ads', 'Data Analytics'],
-      fecha: 'Ayer',
-    },
-    {
-      id: 3,
-      puesto: 'Gerente de Marketing',
-      empresa: 'Saasify Global',
-      ubicacion: 'Remoto',
-      modalidad: 'Tiempo Completo',
-      salario: 1800,
-      descripcion:
-        'Encargado de planificar y optimizar estrategias de adquisición de clientes B2B mediante canales digitales.',
-      tags: ['SEO', 'Google Ads', 'Data Analytics'],
-      fecha: 'Ayer',
-    },
-    {
-      id: 3,
-      puesto: 'Gerente de Marketing',
-      empresa: 'Saasify Global',
-      ubicacion: 'Remoto',
-      modalidad: 'Tiempo Completo',
-      salario: 1800,
-      descripcion:
-        'Encargado de planificar y optimizar estrategias de adquisición de clientes B2B mediante canales digitales.',
-      tags: ['SEO', 'Google Ads', 'Data Analytics'],
-      fecha: 'Ayer',
-    },
-    {
-      id: 3,
-      puesto: 'Gerente de Marketing',
-      empresa: 'Saasify Global',
-      ubicacion: 'Remoto',
-      modalidad: 'Tiempo Completo',
-      salario: 1800,
-      descripcion:
-        'Encargado de planificar y optimizar estrategias de adquisición de clientes B2B mediante canales digitales.',
-      tags: ['SEO', 'Google Ads', 'Data Analytics'],
-      fecha: 'Ayer',
-    },
-  ]);
-}
+export class Empleos {}
