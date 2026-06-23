90986e7e450a02456a76879538c7f062642c0c72
Marycarmen <marycarmen.rivo@gmail.com>
2026-06-22 20:36:31 -0600

    Uso de locale 'es' an app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { providePrimeNG } from 'primeng/config'
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 import { AppTheme } from './themes/app.theme';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs);
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: LOCALE_ID, useValue: 'es' },
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     providePrimeNG({
       theme: {
         preset: AppTheme,
         options: {
-          darkModeSelector: '.app-dark'
-        }
+          darkModeSelector: '.app-dark',
+        },
       },
-      ripple: true
-    })
-  ]
+      ripple: true,
+    }),
+  ],
 };
