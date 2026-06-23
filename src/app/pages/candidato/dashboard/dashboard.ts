f2d26e9f609b96d4cfc94bf58fe9956bbbb205f4
Marycarmen <marycarmen.rivo@gmail.com>
2026-06-22 20:34:25 -0600

    Incorporación de nuevos componentes en dashboard de candidato

--- src/app/pages/candidato/dashboard/dashboard.ts
+++ src/app/pages/candidato/dashboard/dashboard.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { DoughnutChart } from '../../../shared/components/charts/doughnut-chart';
+import { LineChart } from '../../../shared/components/charts/line-chart';
+import { forest } from '../../../themes/palette';
+import { ListaOfertas } from './components/lista-ofertas/lista-ofertas';
+import { AvatarModule } from 'primeng/avatar';
+import { Entrevistas } from './components/entrevistas/entrevistas';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [DoughnutChart],
+  imports: [DoughnutChart, LineChart, ListaOfertas, AvatarModule, Entrevistas, Entrevistas],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
   misPostulacionesData = [
-    { label: 'Enviadas', value: 18, color: '#1e4b38' },
-    { label: 'CV Visto', value: 7, color: '#2e7d53' },
-    { label: 'En Entrevista', value: 4, color: '#a8c5a3' },
+    { label: 'Enviadas', value: 18, color: forest.mid },
+    { label: 'CV Visto', value: 7, color: forest.lite },
+    { label: 'En Entrevista', value: 4, color: forest.pale },
     { label: 'Finalista', value: 2, color: '#d8e3dc' },
   ];
   tituloTooltip = 'postulaciones';
