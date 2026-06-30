9a0622998ba8230c18c3a1f5ae09a3216e0c4620
Marycarmen <marycarmen.rivo@gmail.com>
2026-06-30 12:58:38 -0600

    Utilización de color naranja en landing page

--- src/app/public/pages/landing/components/estadisticas/estadisticas.ts
+++ src/app/public/pages/landing/components/estadisticas/estadisticas.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Analytics } from '../../../../../caption/interfaces/interface';
 import { CaptionService } from '../../../../../caption/services/caption-service';
 
@@ -11,4 +11,23 @@ import { CaptionService } from '../../../../../caption/services/caption-service'
 export class Estadisticas {
   captionS: CaptionService = inject(CaptionService);
   analytics: Analytics = this.captionS.loadAnalytics();
+
+  estadisticas = signal([
+    {
+      title: 'Empresas activas<br />en la plataforma',
+      number: '500+',
+    },
+    {
+      title: 'Candidatos<br />registrados',
+      number: '25k+',
+    },
+    {
+      title: 'Match exitoso<br />basado en datos',
+      number: '92%',
+    },
+    {
+      title: 'Selección<br />más rápida',
+      number: '3x',
+    },
+  ]);
 }
