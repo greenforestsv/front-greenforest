56e351cd258953bee05a3010ffcac090c2d89562
Marycarmen <marycarmen.rivo@gmail.com>
2026-07-01 12:55:59 -0600

    Creación de componente reutilizable green-forest-logo

---
+++ src/app/shared/components/green-forest-logo/green-forest-logo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-green-forest-logo',
+  styleUrl: './green-forest-logo.scss',
+  template: ` <div class="logo"></div> `,
+})
+export class GreenForestLogo {}
